57b9df407d5b424d99599aade005c4dca97c6835
Kunalsing Thakur <kunalsingt@cybage.com>
2021-06-21 14:54:33 +0100

    updating action config

--- build-test.sh
+++ build-test.sh
@@ -2,10 +2,10 @@
 docker_username=$1
 set -xe
 
-curl -sL https://kind.sigs.k8s.io/dl/v0.9.0/kind-linux-amd64 -o /usr/local/bin/kind
+curl -sL https://kind.sigs.k8s.io/dl/v0.11.1/kind-linux-amd64 -o /usr/local/bin/kind
 chmod 755 /usr/local/bin//kind
 
-curl -sL https://storage.googleapis.com/kubernetes-release/release/v1.17.4/bin/linux/amd64/kubectl -o /usr/local/bin/kubectl
+curl -sL https://storage.googleapis.com/kubernetes-release/release/v1.21.1/bin/linux/amd64/kubectl -o /usr/local/bin/kubectl
 chmod 755 /usr/local/bin//kubectl
 
 curl -LO https://get.helm.sh/helm-v3.1.2-linux-amd64.tar.gz
@@ -30,12 +30,15 @@ kubectl apply -f basicapi-service.yaml
 NODE_IP=$(kubectl get node -o wide|tail -1|awk {'print $6'})
 NODE_PORT=$(kubectl get svc basicapi-service -o go-template='{{range.spec.ports}}{{if .nodePort}}{{.nodePort}}{{"\n"}}{{end}}{{end}}')
 sleep 60
-SUCCESS=$(curl $NODE_IP:$NODE_PORT)
-if [[ "${SUCCESS}" != "Hello World" ]]; 
+NODE_IP=$(kubectl get node -o wide|tail -1|awk {'print $6'})
+NODE_PORT=$(kubectl get svc basicapi-service -o go-template='{{range.spec.ports}}{{if .nodePort}}{{.nodePort}}{{"\n"}}{{end}}{{end}}')
+SUCCESS=$(curl --write-out '%{http_code}' --silent --output /dev/null $NODE_IP:$NODE_PORT)
+if [[ "${SUCCESS}" != "200" ]];
 then
- kind -q delete cluster
- exit 1;
+  kind -q delete cluster
+  exit 1;
 else
  kind -q delete cluster
  echo "Component test succesful"
 fi
+
